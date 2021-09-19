f8ce85d89055c675451448c0c415721a1eed6f6b
Stacy-Kucherenko <80332733+Stacy-Kucherenko@users.noreply.github.com>
2021-09-19 12:55:45 +0300

    Add files via upload

---
+++ task2.js
@@ -0,0 +1,12 @@
+menu.onclick = function (){
+  var elements = document.querySelectorAll("li");
+  var visibility = elements[0].style.visibility;
+
+  for (var i =0; i<elements.length; i++){
+    if(visibility == "hidden"){
+      elements[i].style.visibility ="";
+    }else {
+      elements[i].style.visibility = "hidden";
+    }
+  }
+}
